04c72fb71eca6fde7a48492309f9339e04c7bf26
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-09 00:48:57 +0700

    PMSO-41-feat: Update Product Model

--- pmsoService/products/models.py
+++ pmsoService/products/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
 class ProductOrder(models.Model):
     id = models.AutoField(primary_key=True)
     isUrgent = models.BooleanField(default=False)
