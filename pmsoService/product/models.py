3ee3d6d6cb9236dfa9387ac21e972bbabf6a9d01
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-08 23:34:02 +0700

    PMSO-41-feat: Add Model

---
+++ pmsoService/product/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = PhoneNumberField()
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class ProductOrder(models.Model):
+    id = models.AutoField(primary_key=True)
+    isUrgent = models.BooleanField(default=False)
+    dueDate = models.DateField()
+    status = models.CharField(max_length=50)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    saleStaffId = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
