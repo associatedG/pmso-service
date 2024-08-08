b7558207e31f37911114917223f04faeb9bf26f5
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-08 23:53:36 +0700

    PMSO-41-feat: Update Model File

--- pmsoService/product/models.py
+++ pmsoService/product/models.py
@@ -1,20 +1,4 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-
-
-class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone_number = PhoneNumberField()
-
-
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class ProductOrder(models.Model):
