b110d5fb880e92e5fed68b4f65367ba1c0d4c8b2
Tri Nguyen <43049056+rcs-tringuyen@users.noreply.github.com>
2024-08-09 13:23:34 -0700

    Update urls.py

--- pmsoService/pmsoService/urls.py
+++ pmsoService/pmsoService/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("account.urls")),
+    path("api/users/", include("account.urls")),
 ]
