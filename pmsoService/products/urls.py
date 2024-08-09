71046b24120b03c2e7d0e63bea338d6636976d5c
LongNgg <hoanglongnguyen610@gmail.com>
2024-08-09 21:17:07 +0700

    PMSO-115-feat: Design API route and resonse for listing all products

--- pmsoService/products/urls.py
+++ pmsoService/products/urls.py
@@ -1,34 +1,21 @@
 from django.urls import path
-from .views import (
-    ProductListCreateView, ProductRetrieveUpdateDestroyView,
-    ProductOrderListCreateView, ProductOrderRetrieveUpdateDestroyView,
-    ProductOrderProductListCreateView, ProductOrderProductRetrieveUpdateDestroyView
-)
-
-urlpatterns = [
-    # Product endpoints
-    path('products/<uuid:id>/',
-         ProductRetrieveUpdateDestroyView.as_view(),
-         name='product_detail'),
-    path('products/',
-         ProductListCreateView.as_view(),
-         name='product_list_create'),
 
 
-    # ProductOrder endpoints
-    path('productorders/<uuid:id>/',
-         ProductOrderRetrieveUpdateDestroyView.as_view(),
-         name='product_order_detail'),
+""" 
+The following API route is designed to retrieve a list of all products in the database.
+The route is defined at the endpoint '/products/' and is accessible via a GET request.
+When this endpoint is hit, the ProductListView class-based view is invoked.
 
-    path('productorders/',
-         ProductOrderListCreateView.as_view(),
-         name='product_order_list_create'),
+The response from this API will be a JSON array serialized using ProductSerializer, where each element in the array represents
+a single product. Each product object in the array will include the following fields:
+1. id: uuid
+2. name: string
+3. category: string(choice)
+4. quantity: unsigned integer
+5. price: unsigned integer
+6. created_at: DateTime
+7. updated_at: DateTime
+"""
 
-    # ProductOrderProduct endpoints
-    path('productorderproducts/<int:pk>/',
-         ProductOrderProductRetrieveUpdateDestroyView.as_view(), name='product_order_product_detail'),
 
-    path('productorderproducts/', ProductOrderProductListCreateView.as_view(), name='product_order_product_list_create'),
-
-
-]
+urlpatterns = []
