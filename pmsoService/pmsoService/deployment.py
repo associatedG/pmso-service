f8549c11fcea55c00e65c6da0d2bc37025405e1c
Tri Nguyen <43049056+rcs-tringuyen@users.noreply.github.com>
2024-08-09 19:07:44 -0700

    Change whitenoise settings

--- pmsoService/pmsoService/deployment.py
+++ pmsoService/pmsoService/deployment.py
@@ -27,7 +27,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
