5e6fc6a9894b661fc49f121a70e31ce510eedf4a
LongNgg <hoanglongnguyen610@gmail.com>
2024-08-13 11:25:27 +0700

    PMSO-141 added model for Customer and added ForeignKey field for customer in ProductOrder model

--- pmsoService/products/models.py
+++ pmsoService/products/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 import uuid
 
 
@@ -53,28 +54,64 @@ class ProductOrder(models.Model):
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=OPEN)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True, null=True)
+    # Set on_delete to PROTECT to prevent deletion of associated Customer
+    customer = models.ForeignKey(
+        "Customer", on_delete=models.PROTECT, related_name="customer_orders"
+    )
+    last_modified = models.DateTimeField(auto_now=True, null=True)
 
     sale_staff = models.ForeignKey(
         "account.User",
-        on_delete=models.RESTRICT,
+        on_delete=models.PROTECT,
         null=True,
         related_name="sale_orders",
     )
     logistic_staff = models.ForeignKey(
         "account.User",
-        on_delete=models.RESTRICT,
+        on_delete=models.PROTECT,
         null=True,
         related_name="logistic_orders",
     )
     deliverer = models.ForeignKey(
         "account.User",
-        on_delete=models.RESTRICT,
+        on_delete=models.PROTECT,
         null=True,
         related_name="delivery_orders",
     )
 
+    def save(self, *args, **kwargs):
+        if not self.customer:
+            raise ValueError("Customer is required.")
+        super().save(*args, **kwargs)
+
 
 class ProductOrderProduct(models.Model):
-    product_order = models.ForeignKey(ProductOrder, on_delete=models.CASCADE, related_name='products')
+    product_order = models.ForeignKey(
+        ProductOrder, on_delete=models.CASCADE, related_name="products"
+    )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+
+class Customer(models.Model):
+    TIER_1 = "T1"
+    TIER_2 = "T2"
+    TIER_3 = "T3"
+
+    TIER_CHOICES = [
+        (TIER_1, "Cấp độ 1"),
+        (TIER_2, "Cấp độ 2"),
+        (TIER_3, "Cấp độ 3"),
+    ]
+
+    customer_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    phone = PhoneNumberField(region="VN", blank=True)
+    tier = models.CharField(max_length=50, choices=TIER_CHOICES, default=TIER_1)
+    fax = models.IntegerField(blank=True, null=True)
+    contact_list = models.JSONField(blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
