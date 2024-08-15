3981474238129b8128e0bc74dc907f802ada0437
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-15 23:24:03 +0700

    PMSO-169-feat: Update Unit Test

--- pmsoService/products/tests/tests_products.py
+++ pmsoService/products/tests/tests_products.py
@@ -32,18 +32,30 @@ class TestProductsViews(APITestCase):
 	def setUpTestData(self):
 		self.user = User.objects.create_user(username="user", password="test123")
 		self.client = APIClient()
-		self.urls = reverse("product_list_create")
+		self.urls_create = reverse("product_list_create")
 		self.product = mock_product_generator()
 		Product.objects.create(**self.product)
 
 	def test_create_product(self):
 		self.client.force_authenticate(user=self.user)
 		test_product = mock_product_generator()
-		response = self.client.post( self.urls, test_product, format="json")
+		response = self.client.post( self.urls_create, test_product, format="json")
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-		self.assertEqual(Product.objects.count(), 2)
 
 	def test_create_existing_product(self):
 		self.client.force_authenticate(user=self.user)
-		response = self.client.post( self.urls, self.product, format="json")
+		response = self.client.post( self.urls_create, self.product, format="json")
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+	def test_list_product(self):
+		self.client.force_authenticate(user=self.user)
+		response = self.client.get(self.urls_create)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(len(response.data), 1)
+
+	def test_create_invalid_product(self):
+		self.client.force_authenticate(user=self.user)
+		test_create_invalid_product_quantity = mock_product_generator()
+		test_create_invalid_product_quantity.update(quantity = -10)
+		response = self.client.post( self.urls_create, test_create_invalid_product_quantity, format="json")
 		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
