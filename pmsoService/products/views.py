fd47fff98a25f1bdf9616e519465c3f6fe3be408
Dac Nguyen <dacnguyen2101@gmail.com>
2024-08-22 09:25:23 +0700

    PMSO-96-feat: Update docstring and Get Urls

--- pmsoService/products/views.py
+++ pmsoService/products/views.py
@@ -33,6 +33,63 @@ class ProductOrderListCreateView(generics.ListCreateAPIView):
     filterset_class = ProductOrderFilter
     ordering_fields = ['price', 'is_urgent', 'created_at', 'due_date']
     pagination_class = ProductOrderPagination
+    """
+    GET: /api/products/orders?is_urgent=true&due_date__gte=&due_date__lte=&due_date=2024-08-31&status=Open
+    {
+        "count": 1,
+        "next": null,
+        "previous": null,
+        "results": [
+            {
+                "id": "3ec930fe-25e5-4f98-8e6c-3208d2e65053",
+                "is_urgent": true,
+                "due_date": "2024-08-31",
+                "status": "Open",
+                "customer": "65748bd2-9ab4-490e-b124-bcc6b64bc348",
+                "sale_staff": "3a2e5c7c-4231-4fa0-852e-a28aaad21c22",
+                "logistic_staff": "3a2e5c7c-4231-4fa0-852e-a28aaad21c22",
+                "deliverer": "3a2e5c7c-4231-4fa0-852e-a28aaad21c22",
+                "last_modified": "2024-08-21T17:42:40.810884Z",
+                "created_at": "2024-08-21T17:42:11Z",
+                "products": [
+                    {
+                        "product": {
+                            "id": "b516c4f1-7be0-4372-848c-9fb08c858a7d",
+                            "name": "Test Bon Xitec",
+                            "category": "Cơ Khí Ô Tô",
+                            "quantity": 7,
+                            "price": 5
+                        },
+                        "quantity": 1
+                    },
+                    {
+                        "product": {
+                            "id": "8cd1f3bc-bd2e-4fbc-963f-2f4b0e2b35de",
+                            "name": "Test Phuy 1",
+                            "category": "Phuy",
+                            "quantity": 5,
+                            "price": 5
+                        },
+                        "quantity": 1
+                    }
+                ]
+            }
+        ]
+    }
+    POST:
+    {
+        "is_urgent": false,
+        "due_date": null,
+        "status": null,
+        "customer": null,
+        "sale_staff": null,
+        "logistic_staff": null,
+        "deliverer": null,
+        "created_at": null,
+        "products": []
+    }
+    """
+
 
 class ProductOrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ProductOrder.objects.all()
